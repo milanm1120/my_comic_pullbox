b0a3040d446f73df444c073d41e9c93f9882082d
Milan <milanm1120@gmail.com>
2021-10-01 17:19:03 -0400

    :create users table in first migration file

--- db/migrate/20211001191952_create_user.rb
+++ db/migrate/20211001191952_create_user.rb
@@ -1,4 +1,10 @@
 class CreateUser < ActiveRecord::Migration[5.2]
   def change
+      create_table :users do |t|
+        t.string :firstname
+        t.string :lastname
+        t.string :email
+        t.string :username
+        t.stri ng :password_digest
   end
 end
