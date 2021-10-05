90db554dd214532762e5862b7673ff057c9c3352
Milan <milanm1120@gmail.com>
2021-10-05 14:11:53 -0400

    outline ComicbookController

--- app/controllers/comicbook_controller.rb
+++ app/controllers/comicbook_controller.rb
@@ -1,3 +1,39 @@
 class ComicbookController < ApplicationController
+    
+    get "/mycomicbooks" do
+        @comicbooks = Comicbook.all
+        erb :"/comicbook/index"
+    end
+
+    #View A Specific Item
+    get "/mycomicbooks/:id" do
+
+    end
+
+    #Form To Create New Item
+    get "/mycomicbooks/new" do
+
+    end
+
+    #Form To Edit A Specific Item
+    get "/mycomicbooks/:id/edit" do
+
+    end
+
+    #Action To Create New Item
+    post "/mycomicbooks" do
+    
+    end
+
+    #Action To Edit Specific Item
+    patch "/mycomicbooks/:id" do
+    
+    end
+
+    #Delete Specific Item
+    delete "/mycomicbooks/:id" do
+    
+    end
+
 
 end
--- config.ru
+++ config.ru
@@ -4,4 +4,8 @@ if ActiveRecord::Base.connection.migration_context.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
+
+# Add all controllers here!
+
+use ComicbookController
 run ApplicationController
