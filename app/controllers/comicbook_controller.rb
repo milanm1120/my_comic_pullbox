91dc4e82126898756a8d01b15bc660d92416729f
Milan <milanm1120@gmail.com>
2021-10-05 15:05:12 -0400

    code get/:id in comicbook controller and create show.erb

--- app/controllers/comicbook_controller.rb
+++ app/controllers/comicbook_controller.rb
@@ -1,37 +1,38 @@
 class ComicbookController < ApplicationController
     
-    get "/mycomicbooks" do
+    get '/mycomicbooks' do
         @comicbooks = Comicbook.all
-        erb :"/comicbook/index"
+        erb :'/comicbook/index'
+    end
+
+    #Form To Create New Item (new has to go above :id)
+    get '/mycomicbooks/new' do
+        erb :'/comicbook/new'
     end
 
     #View A Specific Item
-    get "/mycomicbooks/:id" do
-
-    end
-
-    #Form To Create New Item
-    get "/mycomicbooks/new" do
-
+    get '/mycomicbooks/:id' do
+        @comicbook = Comicbook.find(params[:id])
+        erb :'/comicbook/show'
     end
 
     #Form To Edit A Specific Item
-    get "/mycomicbooks/:id/edit" do
+    get '/mycomicbooks/:id/edit' do
 
     end
 
     #Action To Create New Item
-    post "/mycomicbooks" do
+    post '/mycomicbooks' do
     
     end
 
     #Action To Edit Specific Item
-    patch "/mycomicbooks/:id" do
+    patch '/mycomicbooks/:id' do
     
     end
 
     #Delete Specific Item
-    delete "/mycomicbooks/:id" do
+    delete '/mycomicbooks/:id' do
     
     end
 
