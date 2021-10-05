06427235958d02cd5ae11018ce92c53e718f4406
Milan <milanm1120@gmail.com>
2021-10-05 13:22:18 -0400

    add seed file

---
+++ app/controllers/comicbook_controller.rb
@@ -0,0 +1,3 @@
+class ComicbookController < ApplicationController
+
+end
---
+++ seeds.rb
@@ -0,0 +1,5 @@
+Comicbook.find_or_create_by(title: "Batman", issue_number: 100, publisher: "DC Comics", year: 2020, writer: "James Tynion IV" , artist: "Jorge Jimenez", cover_price: 6.99, description: "Ending to 'The Joker War'")
+
+Comicbook.find_or_create_by(title: "The Amazing Spider-Man", issue_number: 100, publisher: "Marvel", year: 1971, writer: "Stan Lee" , artist: "Gil Kane", cover_price: 0.15, description: "Peter Parker creates a potion to get rid of his powers.")
+
+Comicbook.find_or_create_by(title: "Superman", issue_number: 100, publisher: "DC Comics", year: 1955, writer: "William Woolfolk" , artist: "Wayne Boring", cover_price: 0.10, description: "Floyd Fowler finds a piece of metal from the rocket that brought Superman to Earth.")
